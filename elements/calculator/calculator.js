13b8d9d669b2f4a721236e06d69d6fb6c76f0e82
blueteeth48 <ayuishii.blue+git@gmail.com>
2014-10-25 16:59:34 -0700

    implement number and clear button

--- elements/calculator/calculator.js
+++ elements/calculator/calculator.js
@@ -1,1 +1,25 @@
-Polymer({});
+Polymer({
+    decimalAdded: false,
+    value1: 0,
+    value2: 0,
+
+    clear: function(event) {
+        this.value1 = 0;
+        this.value2 = 0;
+        this.$.output.innerHTML = 0;
+    },
+
+    numberClick: function(event) {
+        var value = event.currentTarget.value;
+        var currentInput = this.$.output.innerHTML;
+
+        if (currentInput == 0) {
+            this.$.output.innerHTML = value;
+        } else if (value == 'decimal' && !this.decimalAdded) {
+            this.$.output.innerHTML = currentInput + '.';
+            this.decimalAdded = true;
+        } else if (value !== "decimal") {
+            this.$.output.innerHTML = currentInput + value;
+        }
+    }
+});
