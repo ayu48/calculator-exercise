d7ba32840fdb9716af4096047402821a6a108469
blueteeth48 <ayuishii.blue+git@gmail.com>
2014-10-25 17:45:19 -0700

    implement calculate

--- elements/calculator/calculator.js
+++ elements/calculator/calculator.js
@@ -1,12 +1,12 @@
 Polymer({
     decimalAdded: false,
+    operator: null,
     value1: 0,
     value2: 0,
 
     clear: function(event) {
-        this.value1 = 0;
-        this.value2 = 0;
-        this.$.output.innerHTML = 0;
+        this.emptyOutput();
+        this.emptyValues();
     },
 
     numberClick: function(event) {
@@ -14,12 +14,61 @@ Polymer({
         var currentInput = this.$.output.innerHTML;
 
         if (currentInput == 0) {
-            this.$.output.innerHTML = value;
+            this.updateOutput(value);
         } else if (value == 'decimal' && !this.decimalAdded) {
-            this.$.output.innerHTML = currentInput + '.';
+            this.updateOutput(currentInput + '.');
             this.decimalAdded = true;
         } else if (value !== "decimal") {
-            this.$.output.innerHTML = currentInput + value;
+            this.updateOutput(currentInput + value);
         }
+    },
+
+    operatorClick: function(event) {
+        this.value1 = parseFloat(this.$.output.innerHTML);
+        this.operator = event.currentTarget.value;
+        this.decimalAdded = false;
+
+        if (this.operator == 'square-root') {
+            this.calculate();
+        } else {
+            this.emptyOutput();
+        }
+
+    },
+
+    showResult: function() {
+        this.value2 = parseFloat(this.$.output.innerHTML);
+        var result = this.calculate(this.value1, this.value2, this.operator);
+        this.emptyValues();
+        this.updateOutput(result);
+    },
+
+    calculate: function(value1, value2, operator) {
+        var result;
+        if (operator == '+') {
+            result = value1 + value2;
+        } else if (operator == '-') {
+            result = value1 - value2;
+        } else if (operator == 'x') {
+            result = value1 * value2;
+        } else if (operator == '/') {
+            result = value1 / value2;
+        } else if (operator == 'square-root') {
+            result = Math.sqrt(value1);
+        }
+        return result;
+    },
+
+    emptyOutput: function() {
+        this.$.output.innerHTML = 0;
+    },
+
+    emptyValues: function() {
+        this.value1 = 0;
+        this.value2 = 0;
+    },
+
+    updateOutput: function(value) {
+        this.$.output.innerHTML = value;
     }
 });
